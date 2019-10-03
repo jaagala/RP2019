fe1219280c2bf4ba8fbfba3238d10727c0f665d8
jaagala <43170861+jaagala@users.noreply.github.com>
2019-10-03 11:10:42 +0300

    Lesson3: babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: "production",
@@ -9,8 +10,23 @@ module.exports = {
         filename: 'bundle.js'
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new CopyPlugin([{
             from: "public"
         }])
-    ]
+    ],
+    module: {
+        rules: [
+            { 
+            test: /\.js/,
+            exclude: /node_modules/, 
+            use: 'babel-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
 };
