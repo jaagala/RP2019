26383e825f5c77f2282db01b8c2573de753c7a4f
jaagala <43170861+jaagala@users.noreply.github.com>
2019-10-03 10:03:45 +0300

    Lesson2: Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+const homepage = require('./homepage.js');
+const itempage = require('./itempage.js');
+
+
+
 console.log("I am index.");
+
+window.addEventListener("load", () =>{
+    homepage.setup();
+    itempage.setup();   
+});
+
+homepage.setup();
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: "none",
+    mode: "production",
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
