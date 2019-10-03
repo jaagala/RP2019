23eedc0bdff5efdfc399f738205b767ba4da67ff
jaagala <43170861+jaagala@users.noreply.github.com>
2019-10-03 13:05:40 +0300

    Lesson3: jsx problem

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById("app");
+
+ReactDOM.render(
+    <button>I am a button</button>,
+    root
+);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: "production",
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -18,7 +18,7 @@ module.exports = {
     module: {
         rules: [
             { 
-            test: /\.js/,
+            test: /\.(js|jsx)$/,
             exclude: /node_modules/, 
             use: 'babel-loader'
             }
@@ -28,5 +28,5 @@ module.exports = {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 9000
-    }
+    },
 };
